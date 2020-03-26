7932646feb13000b18bf430252d05f322e4d92d2
Jonatas Oliveira <Jonatas.sousa@zup.com.br>
2020-03-26 13:21:18 -0300

    Backend da aplicação

---
+++ backend/src/comentarios.js
@@ -0,0 +1,40 @@
+/**
+ * Get: Buscar/Listar uma informação do back-end.
+ * Post: Criar uma informação no back-end.
+ * Put: Alterar uma informação no back-end.
+ * Delete: Deletar uma informação do back-end.
+ */
+
+/**
+ * Tipos de parametros:
+ * Query params: parametros nomeados enviados na rota, apos "?"(filtros, paginação).
+ * Route params: parametros utilizados para identificar recursos.
+ * Request body: Corpo da requisição, utilizado para criar ou alterar usuario.
+ */
+
+ /**
+  * SQL: MySql, SQLite, PostgreSQL, Oracle, Microsoft SQL Server.
+  * NoSQL: MongoSQL, CouchDB, etc...
+  */
+
+    /**
+    * Driver: Select * FROM users
+   * Query Builder: table('users').select('*').where().
+   */
+
+   /**
+    * Entidades:
+    * ONG
+    * Caso(incident)
+    */
+   /**
+    * Funcionalidades:
+    * Login da ONG
+    * Logout da ONG
+    * Cadastro da ONG
+    * Cadastrar novos casos da ONG
+    * Deletar casos da ONG
+    * Listar casos especificos da ONG
+    * Listar todos os casos
+    * Entrar em contato com a ONG
+    */
